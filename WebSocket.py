1dd0e3016dbfa2bf9d130dc47187ee760826fa14
hannemann <hannemann666@gmx.net>
2020-09-27 01:05:20 +0200

    feat: send messages via websocket instantaneously

--- WebSocket.py
+++ WebSocket.py
@@ -3,43 +3,50 @@ import websockets
 from Config import config, defaults
 import threading
 import json
+from collections import deque
 
 
 class WebSocket(threading.Thread):
 
     connected = set()
+    queue = deque([])
 
     def __init__(self):
         super().__init__()
         self.listen_address = config.get('ws', 'listen_address', fallback=defaults['ws']['listen_address'])
         self.port = config.getint('ws', 'port', fallback=defaults['ws']['port'])
         self.loop = asyncio.get_event_loop()
-        self.server = websockets.serve(self.listen, self.listen_address, self.port)
+        self.server = websockets.serve(self.connect, self.listen_address, self.port)
         self.loop.run_until_complete(self.server)
 
     def run(self):
         print('Websocket: Start listening on %s:%s...' % (self.listen_address, str(self.port)))
+        self.loop.create_task(self.send())
         self.loop.run_forever()
         print('Websocket: Loop stopped')
 
-    async def listen(self, websocket, path):
+    async def connect(self, websocket, path):
         self.connected.add(websocket)
         try:
-            while True:
+            while self.loop.is_running():
                 message = await websocket.recv()
                 print(message)
-        # except ConnectionClosedError:
-        #    pass
+        except websockets.exceptions.ConnectionClosedOK:
+            pass
         finally:
             self.connected.remove(websocket)
 
-    def send(self, message):
-        for websocket in self.connected:
-            self.loop.create_task(websocket.send(message))
+    async def send(self):
+        while self.loop.is_running():
+            if len(self.queue) > 0:
+                message = self.queue.popleft()
+                for websocket in self.connected:
+                    await websocket.send(message)
+            await asyncio.sleep(0.1)
 
     def send_device_stats(self, device):
         stats = device.get_stats()
-        self.send(json.dumps(stats))
+        self.queue.append(json.dumps(stats))
 
     def shutdown(self):
         self.loop.call_soon_threadsafe(self.loop.stop)

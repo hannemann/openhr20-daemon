0312b1904d866501f04295cb920242a798f01a1c
hannemann <hannemann666@gmx.net>
2020-10-05 08:55:50 +0200

    fix: datetime not timezone aware

--- RTC.py
+++ RTC.py
@@ -1,17 +1,21 @@
 from datetime import datetime
+import pytz
+import os
 from SerialIO import serialIO
 
+tz = pytz.timezone(os.getenv('TZ'))
 
-def time():
-    d = datetime.now()
+
+def hex_time():
+    d = datetime.now(tz)
     return 'H{:02x}{:02x}{:02x}{:02x}'.format(d.hour, d.minute, d.second, round(d.microsecond / 10000))
 
 
-def date():
-    d = datetime.now()
+def hex_date():
+    d = datetime.now(tz)
     return 'Y{:02x}{:02x}{:02x}'.format(d.year - 2000, d.month, d.day)
 
 
 def write():
-    serialIO.write(date())
-    serialIO.write(time())
+    serialIO.write(hex_date())
+    serialIO.write(hex_time())
